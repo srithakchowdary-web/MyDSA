05786b5a14ab1dc0d92c68d4fb94ba87624d1d4c
Sritha Chowdary K <srithakchowdary@gmail.com>
2026-07-28 15:05:24 +0530

    Time: 3 ms (75.30%) | Memory: 15.2 MB (50.77%) - LeetSync

---
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> curr;
+    void backtrack(vector<vector<int>>& graph, int src, int dest){
+        curr.push_back(src);
+        if(src==dest) {
+            ans.push_back(curr);
+            curr.pop_back();
+            return;
+        }
+        for(int x : graph[src]){
+            backtrack(graph,x,dest);
+        }
+        curr.pop_back(); 
+    }
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        //stack<int> st;
+        //vector<bool> vis(graph.size(),false);
+        backtrack(graph,0,graph.size()-1);
+        return ans;
+
+    }
+};
