b9f69b3eb7044dfc25ccbf01ba047c1a217159f3
Sritha Chowdary K <srithakchowdary@gmail.com>
2026-07-28 13:58:04 +0530

    Time: 24 ms (80.00%) | Memory: 54.1 MB (91.65%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -4,20 +4,12 @@ public:
         int i,n;
         n = s.size();
         if(n==1) return s;
-        sort(s.begin(),s.end());
+        //sort(s.begin(),s.end());
         vector<int> v(26,0);
-        int count=1,mid;
-        for(i=1;i<n;++i){
-            //idx = s[i]-'a';
-            if(s[i]==s[i-1]) {
-                ++count;
-            }
-            else{
-                v[s[i-1]-'a'] = count;
-                count=1;
-            }
-        }
-        v[s[n-1]-'a']=count;
+        int mid;
+        for(i=0;i<n;++i){
+            v[s[i]-'a']++;
+        } 
         mid = n/2; 
         int j=0,k=n-1;
         for(i=0;i<26;++i){
